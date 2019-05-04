aff57c7a734f4261895a79526a2e634ad6942d72
Paul Tobin <tobster111@gmail.com>
2019-05-04 11:10:32 +0100

    Changed App to class based component and added state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <h1>APP.js</h1>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    language: 'english'
+  };
+  
+  render() {
+    return (
+      <div className="ui container">
+        <h1>App.js</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
