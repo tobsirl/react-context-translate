027066a4830ba745366ad2eec68750e6cc954861
Paul Tobin <tobster111@gmail.com>
2019-05-04 12:22:59 +0100

    Added conditional logic to Field and Button to change output dependent on the language selected

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state.language);
     return (
       <div className="ui container">
         <div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,12 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
   static contextType = LanguageContext;
+
   render() {
-    console.log(this.context)
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
       <div>
-        <button className="ui button primary">Submit</button>
+        <button className="ui button primary">{text}</button>
       </div>
     );
   }
