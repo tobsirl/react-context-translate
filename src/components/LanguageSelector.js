78fa2bd78c50089fa5423a58c278bfaa954ab39c
Paul Tobin <tobster111@gmail.com>
2019-05-10 11:00:29 +0100

    Using static contextType to access the state from LanguageContext

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -3,18 +3,19 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends Component {
   static contextType = LanguageContext;
-  
+
   render() {
+    console.log(this.context);
     return (
       <div>
         Select a language:
         <i
           className="flag us"
-          onClick={() => this.props.onLanguageChange('english')}
+          onClick={() => this.context.onLanguageChange('english')}
         />
         <i
           className="flag nl"
-          onClick={() => this.props.onLanguageChange('dutch')}
+          onClick={() => this.context.onLanguageChange('dutch')}
         />
       </div>
     );
