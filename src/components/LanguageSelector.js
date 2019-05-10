c27b512853da0452845c7c158b58e04b3c99ac2f
Paul Tobin <tobster111@gmail.com>
2019-05-10 10:00:55 +0100

    LanguageSelector passes a prop back up to App.js where the context api sends it to field/button

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -4,7 +4,15 @@ class LanguageSelector extends Component {
   render() {
     return (
       <div>
-        
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.props.onLanguageChange('english')}
+        />
+        <i
+          className="flag nl"
+          onClick={() => this.props.onLanguageChange('dutch')}
+        />
       </div>
     );
   }
