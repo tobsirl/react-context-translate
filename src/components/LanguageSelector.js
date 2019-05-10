1efedbcc926481ce827de5b9e6fabae2af37c3ed
Paul Tobin <tobster111@gmail.com>
2019-05-10 09:54:18 +0100

    Refactored to extract the Language Selection into a seperate class based component

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LanguageSelector extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
